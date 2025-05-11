6996b5519c344529ad219fbb21864e0e0aecfc40
NahuelMart <nahuel.martinez09@outlook.com>
2025-05-11 00:54:33 -0300

    Primer prueba layout

---
+++ Controllers/PedidosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaExpress.Controllers
+{
+    public class PedidosController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Selected"] = "Pedidos"; // Activa el ítem "Otros pedidos" en la navbar
+            return View();
+        }
+    }
+}
---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzaExpress.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Selected"] = "Pizza"; // Marca activo el ítem de navbar
+            return View();
+        }
+    }
+}
